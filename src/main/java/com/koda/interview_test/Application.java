e3e1e71d049c1eaf051f85a0a5964d600505e2ba
KodaKo-WaveGIS <koda_ko@wavegis.com.tw>
2025-04-24 14:54:22 +0800

    init

---
+++ src/main/java/com/koda/interview_test/Application.java
@@ -0,0 +1,13 @@
+package com.koda.interview_test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
